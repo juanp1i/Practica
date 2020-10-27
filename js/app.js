093d35a10fbe895b3b999c04024e3e7115152b60
juanp1i <lopzjuan20@gmail.com>
2020-10-27 15:13:16 -0600

    Practica con JS

---
+++ js/app.js
@@ -0,0 +1,20 @@
+class Libro{
+    constructor(titulo, autor, isbn){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+    }
+    
+    static agregarLibro(libro){
+
+    }
+
+    static mostrarLibros(){
+
+    }
+
+    static eliminarLibro(){
+        
+    }
+}
+
